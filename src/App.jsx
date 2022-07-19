795c6a2ffeb82771a7892a45c8385b74615a741a
Umut Palabiyik <57181469+UmutPalabiyik@users.noreply.github.com>
2022-07-19 16:30:30 +0300

    add validation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 // components
 import SignUpForm from "./components/SignUpForm";
 
+import "./App.css"
+
 function App() {
 
   return (
-    <div className="App flex flex-col items-center justify-center">
+    <div className="App flex flex-col items-center justify-center bg-emerald-100">
       <div className="container max-w-4xl">
         <SignUpForm />
       </div>
--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,6 +1,6 @@
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
-import { Form, Formik, ErrorMessage  } from "formik";
+import { Form, Formik, ErrorMessage } from "formik";
 import Card from "@mui/material/Card";
 import validationSchema from "../validations/SignUpValidation";
 
@@ -17,57 +17,72 @@ const SignUpForm = () => {
   };
 
   return (
-    <Card className="mt-5 px-4 py-8 mx-auto" sx={{ maxWidth: 600 }}>
+    <Card className="mt-5 px-4 py-8 mx-auto drop-shadow-lg max-w-2xl bg-emerald-50">
       <Formik
         initialValues={initialValue}
         onSubmit={handleOnSubmit}
         validationSchema={validationSchema}
       >
-        {({ values, errors, handleChange, handleSubmit }) => (
+        {({ values ,handleChange, handleSubmit }) => (
           <Form className="flex flex-col gap-y-4" onSubmit={handleSubmit}>
+            <TextField
+              value={values.name}
+              onChange={handleChange}
+              label="Name"
+              name="name"
+              variant="outlined"
+              color="success"
+              focused
+            />
+            <ErrorMessage
+              className="text-rose-600 text-xs -mt-3"
+              component="span"
+              name="name"
+            />
+            <TextField
+              value={values.lastname}
+              onChange={handleChange}
+              label="Last Name"
+              name="lastName"
+              variant="outlined"
+              color="success"
+              focused
+            />
+            <ErrorMessage
+              className="text-rose-600 text-xs -mt-3"
+              component="span"
+              name="lastName"
+            />
 
-              <TextField
-                value={values.name}
-                onChange={handleChange}
-                label="Name"
-                name="name"
-                variant="outlined"
-                color="success"
-                focused
-              />
-              <ErrorMessage name="name" />
-              <TextField
-                value={values.lastname}
-                onChange={handleChange}
-                label="Last Name"
-                name="lastName"
-                variant="outlined"
-                color="success"
-                focused
-              />
-              <ErrorMessage name="lastName" />
-       
-              <TextField
-                value={values.password}
-                onChange={handleChange}
-                label="Password"
-                name="password"
-                variant="outlined"
-                color="success"
-                focused
-              />
-              <ErrorMessage name="password" component="span"/>
-              <TextField
-                value={values.confirmPassword}
-                onChange={handleChange}
-                label="Confirm Password"
-                name="confirmPassword"
-                variant="outlined"
-                color="success"
-                focused
-              />
-              <ErrorMessage name="confirmPassword" />
-          
+            <TextField
+              value={values.password}
+              onChange={handleChange}
+              label="Password"
+              name="password"
+              variant="outlined"
+              color="success"
+              focused
+            />
+            <ErrorMessage
+              className="text-rose-600 text-xs -mt-3"
+              component="span"
+              name="password"
+            />
+            <TextField
+              value={values.confirmPassword}
+              onChange={handleChange}
+              label="Confirm Password"
+              name="confirmPassword"
+              variant="outlined"
+              color="success"
+              focused
+            />
+            <ErrorMessage
+              className="text-rose-600 text-xs -mt-3"
+              component="span"
+              name="confirmPassword"
+            />
+
             <Button variant="contained" size="large" type="submit">
               Submit
             </Button>
